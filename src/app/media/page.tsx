b2458e9f58fa3f70f4e2a6509169491b3cb1eca4
Hasna <hasnahena.asiatic@gmail.com>
2024-08-27 15:43:18 +0600

    banner

--- src/app/media/page.tsx
+++ src/app/media/page.tsx
@@ -1,22 +1,11 @@
 "use client"
 import Link from 'next/link';
+import Media from '../components/Media/media';
 
 const MediaPage = () => {
   return (
     <div className="container mx-auto px-4 py-6">
-      <h1 className="text-3xl font-bold mb-4">Media Page</h1>
-      <p className="mb-4">Welcome to the Media page. Here you can find all media-related content.</p>
-      <ul>
-        <li>
-          <Link href="/media/photo-gallery" className="text-blue-500">Photo Gallery</Link>
-        </li>
-        <li>
-          <Link href="/media/video-library" className="text-blue-500">Video Library</Link>
-        </li>
-        <li>
-          <Link href="/media/articles" className="text-blue-500">Articles</Link>
-        </li>
-      </ul>
+      <Media/>
     </div>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,7 +18,9 @@ const config: Config = {
         "2xl": "1400px",
       },
     },
+    
     extend: {
+      
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -81,6 +83,9 @@ const config: Config = {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
+      }, slideIn: {
+        '0%': { transform: 'translateX(100%)' },
+        '100%': { transform: 'translateX(0)' },
       },
       keyframes: {
         "accordion-down": {
